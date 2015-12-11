39b609d47e309ec965d8cfa6fdeb052d23fe3ba4
Tristan Slominski <tristan.slominski@gmail.com>
2015-12-11 10:14:43 -0600

    Problem: Exit codes are not supported.
    Solution: Added support for exit codes.
              Updated to clie@0.2.0.

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 "use strict";
 
-var clie = require('clie');
-var events = require('events');
-var LogTelemetryEvents = require('telemetry-events-log');
-var path = require('path');
-var TelemetryEvents = require('telemetry-events');
+var clie = require("clie");
+var events = require("events");
+var LogTelemetryEvents = require("telemetry-events-log");
+var path = require("path");
+var TelemetryEvents = require("telemetry-events");
 
-var bin = module.exports = function bin(pkg, commandsDirectory) {
+var bin = module.exports = function bin(pkg, commandsDirectory)
+{
     // configure cli telemetry
     var cli = new events.EventEmitter();
-    cli.on('telemetry', function(event) {
+    cli.on("telemetry", function(event)
+    {
         console.log(JSON.stringify(event));
     });
-    var cliTelemetryEmitter = new TelemetryEvents({
+    var cliTelemetryEmitter = new TelemetryEvents(
+    {
         package: pkg,
         emitter: cli
     });
-    var cliTelemetry = new LogTelemetryEvents({
+    var cliTelemetry = new LogTelemetryEvents(
+    {
         telemetry: cliTelemetryEmitter
     });
 
-    process.on('uncaughtException', function(error) {
-        cliTelemetry.log("error", error.message, {
+    process.on("uncaughtException", function(error)
+    {
+        cliTelemetry.log("error", error.message,
+        {
             error: error,
             stack: error.stack,
             uncaught: true
@@ -31,26 +37,39 @@ var bin = module.exports = function bin(pkg, commandsDirectory) {
     // configure app
     var app = clie({commandsDirectory: commandsDirectory}).cli();
 
-    app.on('data', function(data) {
-        if (typeof data === "string") {
+    app.on("data", function(data)
+    {
+        if (typeof data === "string")
+        {
             return process.stdout.write(data);
         }
-
         cliTelemetryEmitter.emit(data);
     });
 
-    app.on('error', function(error) {
-        if (typeof error === "string") {
+    app.on("error", function(error)
+    {
+        if (typeof error === "string")
+        {
             return process.stderr.write(error);
         }
-
         cliTelemetryEmitter.emit(error);
     });
 
-    app.on('end', function() {
-        cliTelemetry.log('info', 'exiting', {
+    app.on("end", function()
+    {
+        cliTelemetry.log("info", "exiting",
+        {
             exit: 0
         });
         process.exit(0);
+    });
+
+    app.on("exit", function(exitCode)
+    {
+        cliTelemetry.log("info", "exiting",
+        {
+            exit: exitCode
+        });
+        process.exit(exitCode);
     });
 };
